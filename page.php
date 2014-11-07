7d45cdb0e6443e55dffb947a84c3de479216f762
Mike Sinkula <mike@mikesinkula.com>
2014-11-06 23:27:05 -0800

    more

--- page.php
+++ page.php
@@ -2,23 +2,22 @@
 
 <!-- Begin Content -->
 <div id="content" class="page-<?php the_ID(); ?>">
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<article id="page-content-<?php the_ID(); ?>" class="page-content">
     <h1><?php the_title(); ?></h1>
     <?php if(has_post_thumbnail()) { echo get_the_post_thumbnail($post->ID, 'large'); }?>
     <?php the_content(); ?>
 	</article>
-<?php endwhile; endif; ?>
+	<?php endwhile; endif; ?>
 
-<?php $page_excerpts = array('post_type' => 'page','numberposts' => -1,'post_status' => null,'post_parent' => $post->ID,'order' => 'ASC','orderby' => 'menu_order'); $child_pages = get_posts($page_excerpts);  ?> 
+	<?php $page_excerpts = array('post_type' => 'page','numberposts' => -1,'post_status' => null,'post_parent' => $post->ID,'order' => 'ASC','orderby' => 'menu_order'); $child_pages = get_posts($page_excerpts);  ?> 
 <?php foreach ($child_pages as $post) : setup_postdata($post); // get child page titles and excerpts ?>
     <article id="page-excerpt-<?php the_ID(); ?>" class="page-excerpt">
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?> &raquo;</a></h2>
     <?php the_excerpt(); ?>
     <p class="full-story"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">Read More &raquo;</a></p>
     </article>
-<?php endforeach; ?>
-
+	<?php endforeach; ?>
 </div>
 <!-- End Content -->
 
